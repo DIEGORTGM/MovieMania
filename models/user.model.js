aad4dd102ea78a3de114736edc8cc5a3ee7e973f
Laura Sanchez <laura695hotmail.com>
2020-07-06 23:51:04 +0200

    cloudinary

--- models/user.model.js
+++ models/user.model.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema
 
 const userSchema = new Schema({
     username: String,
+    imagePath: String,
     password: String
 }, {
     timestamps: true
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user.model')
 
-router.get('/profile', (req, res) => res.render('auth/profile'))
+const cloudUploader = require('../configs/cloudinary.config')
 
+
+router.get('/', (req, res) => {
+    // TO-DO necesito recoger datos del usario de la base de datos
+    res.render('auth/profile')
+    console.log(req.user);
+
+})
+
+
+
+router.post('/imagen', cloudUploader.single('imageFile'), (req, res, next) => {
+    User.findByIdAndUpdate(req.user._id, { imagePath: req.file.url })
+        .then(() => res.redirect('/profile'))
+        .catch(err => next(new Error(err)))
+
+})
 
 
 module.exports = router
+
+
