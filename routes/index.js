e5d74d89e2ca7ec0a67101f9eba006b077cc0f65
Laura Sanchez <laura695hotmail.com>
2020-07-06 19:28:20 +0200

    rutas

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,7 @@ module.exports = app => {
   // Base URLS
   app.use('/', require('./index.routes'))
   app.use('/', require('./auth.routes'))
+  app.use('/movies', require('../routes/movies/movies.routes.js'))
+  app.use('/movies/details/:id', require('../routes/movies/movies.routes.js'))
+  app.use('/profile', require('./user.routes'))
 }
